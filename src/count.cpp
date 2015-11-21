65e831cfb3f7e12422fb16f719c74def7df319be
varshikakamala <kamalareddy159@gmail.com>
2015-11-21 10:19:33 -0800

    Submit Commit

--- src/count.cpp
+++ src/count.cpp
@@ -12,5 +12,11 @@ ERROR CASES: Return -1 in error cases.
 NOTES:
 */
 int count(int num1, int num2) {
-	return 0;
+		if (num1 > 0 && num2 > num1)
+			return (((num2 - num1) / num1) + 1);
+		else if (num1>num2 || num2 == num1)
+			return 0;
+		else
+			return -1;
+
 }
